be961c305420ccd057182808d11ae39705e38f41
douglas <douglas@iMacinWans.local>
2012-05-08 20:14:00 +0800

    Initial Commit

---
+++ StrataChina/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StrataChina
+//
+//  Created by Douglas Wan on 5/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
